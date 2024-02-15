c28b06e868bf6904fc8d4745a178f2d0da83c4ee
belkud <vlad-belkud@yandex.ru>
2024-02-15 23:02:02 +0300

    gh

--- routes/api.ts
+++ routes/api.ts
@@ -1,8 +1,16 @@
 import Router from 'express'
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const router = Router()
 
-// Описываем функцию, которая будет обрабатывать GET запросы на адрес '/'
+//! Описываем функцию, которая будет обрабатывать GET запросы на адрес '/'
+
+
+//!!!!!!!!!! Здесь прописываю логику файла schema.prisma
+
+
+
+
 router.get('/', function (req, res) {
   res.send('Hello from api')
 })
