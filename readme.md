84ab1a2a4404a25ed8be7c951d462c167d50587a
belkud <vlad-belkud@yandex.ru>
2024-02-19 17:11:19 +0300

    posts

--- readme.md
+++ readme.md
@@ -9,6 +9,12 @@ npm init vite@latest <project-name> --template vue
 
 #### 2. Выбираем VUE -> TypeScript 
 #### Пишем cd (название проекта) , npm install , npm run dev
+
+
+#### 3. Основные способы установки VUE (подробная инструкция)
+```
+https://v3.ru.vuejs.org/ru/guide/installation.html#%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F-%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B8
+```
 ---
 ---
 
--- shop/src/components/HelloWorld.vue
+++ shop/src/components/HelloWorld.vue
@@ -6,26 +6,52 @@ defineProps<{ msg: string }>()
 const likes = ref(0)
 const dislikes = ref(0)
 
+const posts = [
+  {id: 1, title:'JS', body: 'Описание поста 1'},
+  {id: 2, title:'JS', body: 'Описание поста 2'},
+  {id: 3, title:'JS', body: 'Описание поста 3'},
+  {id: 4, title:'JS', body: 'Описание поста 4'},
+]
+
+
 
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-  <div @:click="likes--">click 1</div>
-  <div @:click="dislikes++">click 2</div>
+<div class="post" v-for="post in posts">
+  <div><strong>Язык {{ post.title }}</strong><br> Пост о знакомствах {{ post.body }}</div>
+
+
+
+  <!-- <div><strong>Название 1</strong> пост о знакомствах 1</div>
+  <div><strong>Название 2</strong> пост о знакомствах 2</div>
+  <div><strong>Название 3</strong> пост о знакомствах 3</div>
+  <div><strong>Название 4</strong> пост о знакомствах 4</div> -->
+
+</div>
+
+  <div class="clc" @:click="likes--">click 1</div>
+  <div class="clc" @:click="dislikes++">click 2</div>
   <button class="btn" @:click="likes++">лайки {{ likes }}</button>
   <button class="btn" @:click="dislikes--">{{ dislikes }}</button>
 </template>
 
 <style scoped>
+
+.post>div {
+  border: 1px solid lightgreen;
+  padding: 10px;
+  margin: 20px;
+}
   h1, .btn {
     color:yellowgreen;
   }
-  div {
+  .btn, .clc {
     border: 1px solid aliceblue;
     margin-top: 10px;
   }
-  div:hover {
+  .btn:hover, .clc:hover {
     border: 1px solid red;
   }
 </style>
