c98aaa4e667668bed8776439381c0a290b0f507d
belkud <vlad-belkud@yandex.ru>
2024-02-19 14:09:40 +0300

    jj

--- shop/src/components/HelloWorld.vue
+++ shop/src/components/HelloWorld.vue
@@ -3,36 +3,29 @@ import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const likes = ref(0)
+const dislikes = ref(0)
+
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div @:click="likes--">click 1</div>
+  <div @:click="dislikes++">click 2</div>
+  <button class="btn" @:click="likes++">лайки {{ likes }}</button>
+  <button class="btn" @:click="dislikes--">{{ dislikes }}</button>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+  h1, .btn {
+    color:yellowgreen;
+  }
+  div {
+    border: 1px solid aliceblue;
+    margin-top: 10px;
+  }
+  div:hover {
+    border: 1px solid red;
+  }
 </style>
